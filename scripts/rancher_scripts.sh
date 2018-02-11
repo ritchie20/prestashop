658f12bd0202db60b37d05110ffef4c85f295b0c
ritchie20 <oscar@MacBook-Pro-de-Oscar.local>
2018-02-11 18:44:18 -0300

    Added script to install and run rancher

---
+++ scripts/rancher_scripts.sh
@@ -0,0 +1,2 @@
+# Rancher installation with DB mounted on host (bind mount)
+$ sudo docker run -d -v /home/rancher/mount_point/rancher_db:/var/lib/mysql --restart=unless-stopped -p 8080:8080 rancher/server
